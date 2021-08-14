610f51ac394635b268738a09136f365f284eb25e
Quentin <quentin.bonnin@outlook.fr>
2021-08-14 09:52:44 -0400

    Ajout du fichier json

--- js/photographers.js
+++ js/photographers.js
@@ -1,20 +1,29 @@
-import * as photographers from "****.json";
 
-const displayPhotographers = (photographers) => {
+function grabDataFromJSON(){
+  fetch("")
+   .then(response => response.json())
+   .then (dataJson => console.log(dataJson))
+}
+grabDataFromJSON();
+
+
+
+const displayPhotographers = (photographer) => {
   const photographersElement = document.querySelector("#photographers");
 
-  photographersElement.innerHTML = photographers
+  photographersElement.innerHTML = photographer
     .map(
       (photographer) => `
       <div class="photographers__photographer">
-        <a class="photographers__photographer__link" href="/pages/photographer?id=${photographer.id}">
-            <img class="photographers__photographer__img"src="${photographer.imageURL}" alt="${photographer.name}">
-            <h2 class="photographers__photographer__name">${photographer.name}</h2>
+        <a class="photographers__photographer__link" href="/pages/photographer?id=${photographers.id}">
+            <img class="photographers__photographer__img"src="${photographers.portrait}" alt="${photographers.name}">
+            <h2 class="photographers__photographer__name">${photographers.name}</h2>
         </a>
-        <h3 class="photographers__photographer__localisation">${photographer.localisation}</h3>
-        <p class="photographers__photographer__description">${photographer.description}</p>
-        <p class="photographers__photographer__price">${photographer.pricing}</p>
+        <h3 class="photographers__photographer__localisation">${photographers.localisation}</h3>
+        <p class="photographers__photographer__description">${photographers.description}</p>
+        <p class="photographers__photographer__price">${photographers.pricing}</p>
     </div>`
     )
     .join("");
 };
+displayPhotographers();
